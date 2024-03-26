83748f63ed8cef4a80928c348dbfad84cdad6a37
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-03-26 11:58:36 -0700

    fix betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,19 +5,19 @@ size_t height_bt(const binary_tree_t *tree);
 /**
  * binary_tree_height - measures the height of a binary tree
  *
- * tree:  is a pointer to the root node of the tree to measure the height.
+ * @tree:  is a pointer to the root node of the tree to measure the height.
  *
  * Return: the measured height
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t height;
-        
-        if (tree == NULL)
-                return (0);
-        height = height_bt(tree);
-        return (height - 1);
+	size_t height;
+
+	if (tree == NULL)
+		return (0);
+	height = height_bt(tree);
+	return (height - 1);
 }
 
 /**
@@ -29,14 +29,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 size_t height_bt(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
-        
-        if (tree == NULL)
-                return (0);
-        left_height = height_bt(tree->left);
-        right_height = height_bt(tree->right);
+	size_t left_height, right_height;
 
-        if (right_height > left_height)
-                return (right_height + 1);
-        return (left_height + 1);
+	if (tree == NULL)
+		return (0);
+	left_height = height_bt(tree->left);
+	right_height = height_bt(tree->right);
+
+	if (right_height > left_height)
+		return (right_height + 1);
+	return (left_height + 1);
 }
