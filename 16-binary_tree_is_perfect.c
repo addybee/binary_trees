ed2882c109b9621c5677af8c46e77a18b34e838e
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-03-27 09:43:35 -0700

    fix task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+int num_nodes(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_perfect - check if a binary tree is perfect from(node=2^h-1)
@@ -13,29 +14,29 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	/* get the number of nodes in the tree */
-	int size_tree = (int)binary_tree_size(tree);
-	/* check and return if the size is a power of 2 */
+	int size_tree = num_nodes(tree);
+	/* check and return if the size is a power of two */
 	if ((size_tree & (size_tree + 1)) == 0)
 		return (1);
 	return (0);
 }
 
 /**
-* binary_tree_size - a function that measures the size of a binary tree
+* num_nodes- a function that measures the size of a binary tree
 *
 * @tree: a pointer to the root node of the tree to measure the size
 *
 * Return: return 0 if tree is NULL
 */
-size_t binary_tree_size(const binary_tree_t *tree)
+int num_nodes(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	int size = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	size = binary_tree_size(tree->left) + 1
-	+ binary_tree_size(tree->right);
+	size = num_nodes(tree->left) + 1
+	+ num_nodes(tree->right);
 
 	return (size);
 }
